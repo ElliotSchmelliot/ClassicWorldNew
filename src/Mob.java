6217073da1b5a2f00f68e243bfb2f351c98897bb
James Murphree <jamesus95@gmail.com>
2012-11-08 11:51:57 -0800

    added get methods to mob

--- src/Mob.java
+++ src/Mob.java
@@ -6,4 +6,20 @@ public abstract class Mob {
 	//Item[] inventory;
 	String name;
 	
+	public int getLevel() {
+		return level;
+	}
+	
+	public int getHealthMax() {
+		return healthMax;
+	}
+	
+	public int getHealthCurrent() {
+		return healthCurrent;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
 }
