ed52a25564c0e250a11848a06e1f3db6790848a5
Elliot Keder <Osa28@comcast.net>
2012-11-08 11:55:04 -0800

    stuff

--- src/main/TestMain.java
+++ src/main/TestMain.java
@@ -3,7 +3,7 @@ package main;
 import items.EquipmentSet;
 
 public class TestMain {
-	
+	//Elliot
 	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException {
 		EquipmentSet test = new EquipmentSet();
 		System.out.println(test);
