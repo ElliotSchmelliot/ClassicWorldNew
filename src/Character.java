6f892eada962c5afaac7a67b585b1095e3bafb86
Elliot Keder <Osa28@comcast.net>
2012-11-08 12:09:04 -0800

    Character fields

--- src/Character.java
+++ src/Character.java
@@ -1,4 +1,21 @@
 
 public class Character extends Mob {
+	int strength;
+	int intelligence;
+	int agility;
+	//Ability[] attacks;
+	//Ability[] spells;
+	//Ability[] stealth;
+	//Ability[] equippedAbilities;
+	//Armor[] equipment;
+
+	public Character() {
+		strength = 1;
+		intelligence = 1;
+		agility = 1;
+		//set Ability arrays size to 1
+		//equippedAbilities size = 2
+		//Starting equipment?
+	}
 
 }
