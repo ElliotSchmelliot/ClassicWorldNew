4fd36eee1952afa42d6499d5e4ac11066b9b0e53
James Murphree <jamesus95@gmail.com>
2012-11-16 00:53:44 -0800

    Revert "Revert "testMain""
    This reverts commit 72f2ed948a360fdf2040616291cbaa1f031588f8.

--- src/main/TestMain.java
+++ src/main/TestMain.java
@@ -3,8 +3,7 @@ package main;
 import items.EquipmentSet;
 
 public class TestMain {
-	//Elliot
-	//james
+	
 	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException {
 		EquipmentSet test = new EquipmentSet();
 		System.out.println(test);
