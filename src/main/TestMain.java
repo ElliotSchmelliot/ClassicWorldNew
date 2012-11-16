800c5a504994f17fde82d7ba8755ed018395e8ac
James Murphree <jamesus95@gmail.com>
2012-11-16 00:26:53 -0800

    f

--- src/main/TestMain.java
+++ src/main/TestMain.java
@@ -3,7 +3,7 @@ package main;
 import items.EquipmentSet;
 
 public class TestMain {
-
+	
 	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException {
 		EquipmentSet test = new EquipmentSet();
 		System.out.println(test);
