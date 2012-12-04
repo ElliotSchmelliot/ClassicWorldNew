80ba21307c8acd0e965576c07f8980fa5273791e
JMurphs <jamesus95@gmail.com>
2012-12-04 10:30:40 -0800

    bullshitting in testmain

--- src/main/TestMain.java
+++ src/main/TestMain.java
@@ -1,12 +1,38 @@
 package main;
 
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.math.*;
+
 import items.EquipmentSet;
 
 public class TestMain {
-	
-	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException {
-		EquipmentSet test = new EquipmentSet();
-		System.out.println(test);
-	}
 
+	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException {
+		int x = 1000001;
+		int i = 6;
+		boolean prime = false;
+		//Scanner input = new Scanner (System.in);
+		//System.out.println("1\n2\n3\n5");
+		for (;;) {
+			prime = true;
+			i = x / 2;
+			while (prime == true && i != 1.0) {
+				if (x % i == 0) {
+					prime = false;
+				} else {
+					i--;
+				}
+			}
+			if (prime == true) {
+				System.out.println(x);
+			}
+			if ((x - 3) % 10 == 0) {
+				x += 4;
+			} else {
+				x += 2;
+			}
+		}
+	}
 }
+
